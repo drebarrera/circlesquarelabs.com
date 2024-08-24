736d0da0a1df3c0f1bdbb2a68bc4be9ee2fac0d0
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2024-08-24 16:24:45 +0700

    nearly finished. Requires mobile-resizing and scrolling hero

--- components/index/final.js
+++ components/index/final.js
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import { Icon } from "@/components/icon";
 import tools from "@/data/tools.json";
+import links from "@/data/links.json";
 import styles from "@/styles/index.module.css";
 
 export const Final = () => {
     return (
-        <section id="tools" className="h-fit w-full flex flex-col justify-center items-center py-[40px] px-[25px] gap-[40px]" style={ {background: 'linear-gradient(170deg, rgba(25,53,78,0) 20%, rgba(0,255,255,0.25) 80%)'} }>
-            <div className="flex flex-col justify-center items-center gap-[5px]">
+        <section id="tools" className="h-fit w-full flex flex-col justify-center items-center py-[40px] gap-[40px]" style={ {background: 'linear-gradient(170deg, rgba(25,53,78,0) 20%, rgba(0,255,255,0.4) 55%, rgba(25,53,78,0)) 100%'} }>
+            <div className="flex flex-col justify-center items-center gap-[5px] px-[25px] ">
                 <h3 className="text-white text-3xl font-medium">Our Toolkit For</h3>
                 <h3 className="text-white text-5xl font-semibold">Design, Development, & Deployment</h3>
             </div>
-            <div className={`relative h-[330px] w-fill flex flex-row justify-center items-center flex-wrap gap-[20px] ${styles.tools} overflow-clip`} style={ {} }>
+            <div className={`relative h-[330px] w-fill flex flex-row justify-center items-center flex-wrap gap-[20px] px-[25px] ${styles.tools} overflow-clip`} style={ {} }>
                 {
                     Object.entries(tools).map(([index, value]) => {
                         return <Icon
@@ -23,7 +24,7 @@ export const Final = () => {
             </div>
             <div className="relative w-full h-[800px] flex items-center justify-center">
                 <div className="h-full w-full max-w-[1800px] py-[50px] flex flex-row pr-[100px]">
-                    <div className="h-full w-full flex justify-end items-center">
+                    <div className="h-full w-full flex justify-start items-center">
                         <div className="h-full w-full max-w-[65%] relative">
                             <Image
                                 src="/assets/index/hero/code_example.png"
@@ -33,10 +34,11 @@ export const Final = () => {
                             />
                         </div>
                     </div>
-                    <div className="absolute top-0 left-0 pr-[4%] h-full w-full flex flex-col justify-center gap-[20px]">
-                        <div className="flex flex-col justify-center gap-[10px]">
-                            <h2 className="text-white text-5xl font-semibold" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Ready to Plan, Build, and Deploy</h2>
+                    <div className="absolute top-0 left-0 pr-[4%] h-full w-full flex flex-col justify-center items-end gap-[20px] px-[25px] ">
+                        <div className="w-[500px] flex flex-col justify-center gap-[10px]">
+                            <h2 className="text-white text-5xl text-right font-semibold" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Ready to Plan, Build, and Deploy</h2>
                         </div>
+                        <a href={links['inquire']} className="text-white text-2xl font-medium link">Let's get in touch&nbsp;<span className="font-sans">&rarr;</span></a>
                         <img src="/assets/brand/cs-icon-color.svg" alt="CircleSquare Icon" width="75" height="75" />
                     </div>
                 </div>
--- components/index/product_lab.js
+++ components/index/product_lab.js
@@ -36,13 +36,25 @@ export const ProductLab = () => {
                         })
                     }
                 </div>
-                <div className="w-[775px] h-[275px] p-[15px] bg-[rgba(13,13,13,0.7)] flex flex-row gap-[25px]" style={ {'borderRadius': '15px'} }>
+                <div className="w-[775px] h-[340px] p-[15px] bg-[rgba(13,13,13,0.7)] flex flex-row gap-[25px]" style={ {'borderRadius': '15px'} }>
                     <div className="h-[125px] w-[125px] flex flex-col justify-center items-center gap-[5px] cursor-pointer" style={ {'background': `linear-gradient(135deg, ${selected["bg-start"]} 0%, ${selected["bg-stop"]} 100%)`, 'borderRadius': '10px', 'boxShadow': '0px 4px 6px rgba(0, 0, 0, 0.1)'} }>
                         { (selected["title"] == "CircleSquare Labs") ? <img src="/assets/brand/cs-icon-color.svg" alt="CircleSquare Icon" width="70" height="70" /> : <Icon icon={selected["icon"]} classData={`w-[75px] h-[75px] ${styles.icon}`} /> }
                     </div>
                     <div className="w-[595px] flex flex-col gap-[10px]">
                         <h4 className="text-white text-3xl font-medium">{ selected["title"] }</h4>
-                        <p className="text-[#D9D9D9] text-xl">{ selected["description"] }</p>
+                        <p className="h-[190px] overflow-clip text-[#D9D9D9] text-xl">{ selected["description"] }</p>
+                        <div className="relative w-[460px] h-fit flex flex-row justify-center items-center gap-[20px] py-[5px]">
+                            {
+                                Object.entries(selected["tools"]).map(([index, value]) => {
+                                    return <Icon
+                                        key={index}
+                                        icon={value}
+                                        classData={`h-[40px] w-[40px] ${styles.tool}`}
+                                    />
+                                })
+                            }
+                            <div className="absolute top-0 left-0 w-full h-full" style={ {background: 'linear-gradient(to right, rgba(13,13,13,0.8) 0%, rgba(13,13,13,0.05) 30%, rgba(13,13,13,0.05) 70%, rgba(13,13,13,0.8) 100%)'} }></div>
+                        </div>
                     </div>
                 </div>
             </div>
