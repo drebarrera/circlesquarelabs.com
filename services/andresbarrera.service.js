3e824c4c70d7babd6bedcb4b2bd46bab469d0f5a
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2025-01-19 18:11:54 -0600

    Profile update

---
+++ services/andresbarrera.service.js
@@ -0,0 +1,12 @@
+import Airtable from 'airtable';
+
+export async function getAndresBarrera() {
+  const base = new Airtable({ apiKey: process.env.AIRTABLE_PAT }).base('appt5ugZNETWOTJQG');
+
+  const records = await base('Contractors List').select({
+    maxRecords: 1,
+    fields: ["First Name", "Last Name", "Title", "Bio", "Photo", "Skills"],
+    filterByFormula: "{Email} = 'drebarrera@yahoo.com'"
+  }).firstPage();
+  return records.map(record => record.fields)[0];
+}
