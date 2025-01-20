de667cf0f416c3dd23593446ca8368117284d17c
Andres Barrera <63563993+drebarrera@users.noreply.github.com>
2025-01-20 09:11:51 -0600

    Update for mobile version of profile page

--- pages/andresbarrera.js
+++ pages/andresbarrera.js
@@ -17,7 +17,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function AndresBarrera({ andresData }) {
     return (
-        <main className={`flex min-h-screen flex-col items-center gap-[80px] ${inter.className}`} style={{ background: 'linear-gradient(170deg, rgba(25,53,78,0) 20%, rgba(0,255,255,0.4) 55%, rgba(25,53,78,0)) 80%' }}>
+        <main className={`flex overflow-clip min-h-screen flex-col items-center gap-[80px] ${inter.className}`} style={{ background: 'linear-gradient(170deg, rgba(25,53,78,0) 20%, rgba(0,255,255,0.4) 55%, rgba(25,53,78,0)) 80%' }}>
             <Header position="fixed" />
             <section className="flex flex-col justify-center items-center gap-[40px] mt-[140px]">
                 <div className="relative w-[250px] h-[250px] md:w-[300px] md:h-[300px]" >
@@ -25,6 +25,7 @@ export default function AndresBarrera({ andresData }) {
                         src={'/assets/index/team/andres_barrera.webp'} 
                         alt={'Andres Barrera'}
                         fill
+                        priority
                         style={ {'borderRadius': '150px', 'boxShadow': '0px 20px 30px rgba(0, 0, 0, 0.8)', 'objectFit': 'contain'} }
                     />
                 </div>
@@ -54,7 +55,7 @@ export default function AndresBarrera({ andresData }) {
             </section>
             <section className="flex flex-col gap-[20px] md:gap-[30px] m-[50px] mb-[20px] md:mb-[80px]">
                 <a href="https://www.circlesquarelabs.com" target="_blank" className="flex flex-wrap flex-row gap-[30px] justify-center items-center hover:bg-white px-[40px] py-[20px] pb-[80px] md:pb-[20px] hover:text-black transition" style={ {'borderRadius': '180px'} }>
-                    <div className="relative w-[350px] h-[196.875px] md:w-[500px] md:h-[281.25px]" >
+                    <div className="relative w-[300px] h-[168.75px] md:w-[500px] md:h-[281.25px]" >
                         <Image 
                             src={'/assets/andresbarrera/circlesquarelabs_cover.webp'} 
                             alt={'Circle Square Labs'}
@@ -62,13 +63,13 @@ export default function AndresBarrera({ andresData }) {
                             style={{ 'borderRadius': '10px', 'boxShadow': '0px 20px 30px rgba(0, 0, 0, 0.8)', 'objectFit': 'fill'}}
                         />
                     </div>
-                    <div className="flex flex-col w-[350px] md:w-[480px] gap-[10px]">
+                    <div className="flex flex-col w-[300px] md:w-[480px] gap-[10px]">
                         <h3 className="text-2xl">CircleSquare Labs | Senior Software Engineer</h3>
                         <p>CircleSquare Labs is a full-service technology firm with a team of engineering experts to delivery quality products. As a Senior Software Engineer and Project Manager, I lead and service both internal R&D and external software projects.</p>
                     </div>
                 </a>
                 <a href="https://www.hispanichackers.org" target="_blank" className="flex flex-wrap flex-row gap-[30px] justify-center items-center hover:bg-white px-[40px] py-[20px] pb-[80px] md:pb-[20px] hover:text-black transition" style={ {'borderRadius': '180px'} }>
-                <div className="relative w-[350px] h-[196.875px] md:w-[500px] md:h-[281.25px]" >
+                <div className="relative w-[300px] h-[168.75px] md:w-[500px] md:h-[281.25px]" >
                         <Image 
                             src={'/assets/andresbarrera/hh_cover.webp'} 
                             alt={'Hispanic Hackers'}
@@ -76,13 +77,13 @@ export default function AndresBarrera({ andresData }) {
                             style={{ 'borderRadius': '10px', 'boxShadow': '0px 20px 30px rgba(0, 0, 0, 0.8)', 'objectFit': 'fill' }}
                         />
                     </div>
-                    <div className="flex flex-col w-[350px] md:w-[480px] gap-[10px]">
+                    <div className="flex flex-col w-[300px] md:w-[480px] gap-[10px]">
                         <h3 className="text-2xl">Hispanic Hackers | President</h3>
                         <p>Hispanic Hackers is an Austin-based 501(c)(3) non-profit making the tech industry accessible to all Latinos. As President, I oversee our community of over 2,500 members, offering developer & entrepreneur resources and creating professional opportunities</p>
                     </div>
                 </a>
                 <a href="https://hispanichackers.fillout.com/t/fQAwcfMfHrus" target="_blank" className="flex flex-wrap flex-row gap-[30px] justify-center items-center hover:bg-white px-[40px] py-[20px] pb-[80px] md:pb-[20px] hover:text-black transition" style={ {'borderRadius': '180px'} }>
-                <div className="relative w-[350px] h-[196.875px] md:w-[500px] md:h-[281.25px]" >
+                <div className="relative w-[300px] h-[168.75px] md:w-[500px] md:h-[281.25px]" >
                         <Image 
                             src={'/assets/andresbarrera/code_foundry_cover.webp'} 
                             alt={'Code Foundry Product Development Masterclass'}
@@ -90,7 +91,7 @@ export default function AndresBarrera({ andresData }) {
                             style={{ 'borderRadius': '10px', 'boxShadow': '0px 20px 30px rgba(0, 0, 0, 0.8)', 'objectFit': 'fill'}}
                         />
                     </div>
-                    <div className="flex flex-col w-[350px] md:w-[480px] gap-[10px]">
+                    <div className="flex flex-col w-[300px] md:w-[480px] gap-[10px]">
                         <h3 className="text-2xl">Code Foundry | Director</h3>
                         <p>Code Foundry is a Product Development Masterclass created for startup founders and engineers looking to bridge the gap between concept and market-ready product. As the Director, I organize events, coordinate with industry experts, and help founders achieve their startup goals.</p>
                     </div>
